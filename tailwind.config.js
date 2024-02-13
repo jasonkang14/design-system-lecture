15bf3bcd5bb917d20c4e14993fcf90256bf2e99d
Jason Kang <jasonkang14@gmail.com>
2024-02-13 20:10:57 +0900

    configure fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      body: ["Noto Sans KR"],
+    },
     fontSize: {
       xs: [
         "12px",
