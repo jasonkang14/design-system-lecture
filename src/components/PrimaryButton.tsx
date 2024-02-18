ebe12b5e3b7bd0caf9dec2f3805aee8a47157c54
Jason Kang <jasonkang14@gmail.com>
2024-02-18 20:11:21 +0900

    Create a primary button story

---
+++ src/components/PrimaryButton.tsx
@@ -0,0 +1,44 @@
+type PrimaryButtonTheme = "dark" | "light" | "social" | "text";
+
+interface IPrimaryButtonProps {
+  theme: PrimaryButtonTheme;
+  disabled: boolean;
+  children: string;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+const dark = "bg-primary text-white";
+const light = "bg-white text-primary";
+const social = "bg-social text-white";
+const text = "bg-transparent text-white";
+const disabledStyle = "disabled:bg-mono100 disabled:text-mono200";
+
+const color: Record<PrimaryButtonTheme, string> = {
+  dark,
+  light,
+  social,
+  text,
+};
+
+export default function PrimaryButton({
+  theme,
+  children,
+  onClick,
+  disabled,
+}: IPrimaryButtonProps) {
+  return (
+    <button
+      className={`
+        rounded-primary-button
+        w-full 
+        h-[59px] 
+        ${disabledStyle}
+        ${color[theme]}
+      `}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
---
+++ src/stories/PrimaryButton.stories.tsx
@@ -0,0 +1,84 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import PrimaryButton from "../components/PrimaryButton";
+
+const meta = {
+  title: "Buttons/PrimaryButton",
+  component: PrimaryButton,
+  parameters: {
+    layout: "centered",
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ width: "360px" }}>
+        <Story />
+      </div>
+    ),
+  ],
+  tags: ["autodocs"],
+  argTypes: {
+    theme: {
+      control: {
+        type: "select",
+        options: ["dark", "light", "social", "text"],
+      },
+      description: "버튼 테마",
+      defaultValue: "dark",
+    },
+    children: {
+      control: "text",
+      description: "버튼 text",
+      defaultValue: "icon",
+    },
+    disabled: {
+      control: "boolean",
+      description: "버튼 비활성화 여부",
+      defaultValue: true,
+    },
+
+    onClick: { action: "clicked", description: "버튼 클릭 이벤트" },
+  },
+} satisfies Meta<typeof PrimaryButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Dark: Story = {
+  args: {
+    children: "Button",
+    theme: "dark",
+    disabled: false,
+  },
+};
+
+export const Light: Story = {
+  args: {
+    children: "Button",
+    theme: "light",
+    disabled: false,
+  },
+};
+
+export const Social: Story = {
+  args: {
+    children: "Button",
+    theme: "social",
+    disabled: false,
+  },
+};
+
+export const Text: Story = {
+  args: {
+    children: "Button",
+    theme: "text",
+    disabled: false,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    children: "Button",
+    theme: "dark",
+    disabled: true,
+  },
+};
